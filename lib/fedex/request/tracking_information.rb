61e89d52b15d9bc6beb18286b99b1d34b6e58011
CJ <chirag7jain@gmail.com>
2014-10-17 13:47:24 +0530

    getting all waybills associated with tracking number
    Conflicts:
    
    	lib/fedex/request/tracking_information.rb
    	spec/lib/fedex/track_spec.rb

--- lib/fedex/request/tracking_information.rb
+++ lib/fedex/request/tracking_information.rb
@@ -32,7 +32,18 @@ module Fedex
         if success?(response)
           options = response[:track_reply][:track_details]
 
-          Fedex::TrackingInformation.new(options)
+          if response[:track_reply][:duplicate_waybill].downcase == 'true'
+            shipments = []
+            [options].flatten.map do |details|
+              options = {:tracking_number => @package_id, :uuid => details[:tracking_number_unique_identifier]}
+              shipments << Request::TrackingInformation.new(@credentials, options).process_request
+            end
+            shipments
+          else
+            [options].flatten.map do |details|
+              Fedex::TrackingInformation.new(details)
+            end
+          end
         else
           error_message = if response[:track_reply]
             response[:track_reply][:notifications][:message]
